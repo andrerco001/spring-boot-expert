752a96075b7bd04d1a76569051c09d3019869c1b
Andre <andreoliveira.adv@hotmail.com>
2020-12-12 21:01:48 -0500

    Creation of the first test control - hello world

--- vendas/src/main/java/ca/andre/spgboot/application/VendasApplication.java
+++ vendas/src/main/java/ca/andre/spgboot/application/VendasApplication.java
@@ -2,12 +2,19 @@ package ca.andre.spgboot.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class VendasApplication {
-    
+public class VendasApplication
+{
+    @GetMapping("/hello")
+    public String helloWorld()
+    {
+        return "Hello World!!!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
