82d7d48b12bc396d14d7ddf38591ee4ad45e0597
Andre <andreoliveira.adv@hotmail.com>
2020-12-20 18:04:58 -0500

    Finding by name, updating and deleting customers

--- vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
+++ vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
@@ -12,22 +12,53 @@ import ca.andre.spgboot.application.domain.entity.Customer;
 import ca.andre.spgboot.application.domain.repository.CustomerRepository;
 
 @SpringBootApplication
-public class SalesApplication
-{
+public class SalesApplication {
 	@Bean
-	public CommandLineRunner init (@Autowired CustomerRepository customerRepository) {
+	public CommandLineRunner init(@Autowired CustomerRepository customerRepository) {
 		return args -> {
+
+			// Create
+			System.out.println("Creating customers");
 			customerRepository.save(new Customer("Andre"));
-			
 			customerRepository.save(new Customer("Lilys"));
-			
+
 			List<Customer> allCustomers = customerRepository.findAll();
 			allCustomers.forEach(System.out::println);
+
+			// Read
+			System.out.println("Find by name");
+			customerRepository.findByName("Lil").forEach(System.out::println);
+
+			// Update
+			System.out.println("Update");
+			allCustomers.forEach(c -> {
+				c.setName(c.getName() + " atualizado");
+				customerRepository.update(c);
+			});
+
+			// Fid all
+			System.out.println("Find all customers");
+			allCustomers = customerRepository.findAll();
+			allCustomers.forEach(System.out::println);
+
+			// Delete
+//			System.out.println("Deleting customers");
+//			customerRepository.findAll().forEach(c -> {
+//				customerRepository.delete(c.getId());
+//			});
+
+			allCustomers = customerRepository.findAll();
+			if (allCustomers.isEmpty()) {
+				System.out.println("Customers not found!");
+
+			} else {
+				allCustomers.forEach(System.out::println);
+			}
+
 		};
 	}
-	
-    public static void main(String[] args) 
-    {
-        SpringApplication.run(SalesApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApplication.class, args);
+	}
 }
--- vendas/src/main/java/ca/andre/spgboot/application/domain/repository/CustomerRepository.java
+++ vendas/src/main/java/ca/andre/spgboot/application/domain/repository/CustomerRepository.java
@@ -12,31 +12,66 @@ import org.springframework.stereotype.Repository;
 import ca.andre.spgboot.application.domain.entity.Customer;
 
 @Repository
-public class CustomerRepository {
-	
+public class CustomerRepository 
+{
 	private static String INSERT = "insert into customers (name) values (?) ";
 	private static String SELECT_ALL = "select * from customers ";
-	
+	private static String UPDATE = "update customers set name = ? where id = ? ";
+	private static String DELETE = "delete from customers where id = ? ";
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
-	public Customer save(Customer customer) {
-		
-		jdbcTemplate.update(INSERT, new Object[] {customer.getName()});
-		
+
+	// Create
+	public Customer save(Customer customer) 
+	{
+		jdbcTemplate.update(INSERT, new Object[] { customer.getName() });
+
 		return customer;
 	}
 	
-	public List<Customer> findAll(){
-		return jdbcTemplate.query(SELECT_ALL, new RowMapper<Customer>() {
+	// find Customers Mapper
+	private RowMapper<Customer> findCustomersMapper() {
+		return new RowMapper<Customer>() 
+		{
 			@Override
-			public Customer mapRow(ResultSet resultSet, int i) throws SQLException{
-				
+			public Customer mapRow(ResultSet resultSet, int i) throws SQLException 
+			{
 				Integer id = resultSet.getInt("id");
 				String name = resultSet.getString("name");
-				
+
 				return new Customer(id, name);
 			}
-		});
+		};
 	}
+
+	// Read - find all
+	public List<Customer> findAll() 
+	{
+		return jdbcTemplate.query(SELECT_ALL, findCustomersMapper());
+	}
+		
+	// Read - find by name
+	public List<Customer> findByName(String name){
+		
+		return jdbcTemplate.query(
+				SELECT_ALL.concat(" where name like ? "),
+				new Object[] {"%" + name + "%"},
+				findCustomersMapper());
+	}
+
+	// Update
+	public Customer update(Customer customer) 
+	{
+		jdbcTemplate.update(UPDATE, new Object[] { customer.getName(), customer.getId() });
+
+		return customer;
+	}
+
+	// Delete
+	public void delete(Integer id) 
+	{
+		jdbcTemplate.update(DELETE, new Object[] { id });
+	}
+
 }
