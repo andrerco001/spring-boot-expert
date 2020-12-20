a8bfd1564c4daff62de6b77c073f4f41d1dd91be
Andre <andreoliveira.adv@hotmail.com>
2020-12-19 22:18:16 -0500

    Working with multiple environments and profiles

--- vendas/src/main/java/ca/andre/spgboot/application/ProjectConfiguration.java
+++ vendas/src/main/java/ca/andre/spgboot/application/ProjectConfiguration.java
@@ -1,6 +1,20 @@
 package ca.andre.spgboot.application;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
 public class ProjectConfiguration
 {
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("RUN DEVLOPMENT'S CONFIGURATION");
+			
+		};
+	}
 
 }
--- vendas/src/main/java/ca/andre/spgboot/application/VendasApplication.java
+++ vendas/src/main/java/ca/andre/spgboot/application/VendasApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication
 {
-    @Value("${spring.application.name}")
+	@Value("${spring.application.name}")
     private String applicationName;
 
     @GetMapping("/hello")
