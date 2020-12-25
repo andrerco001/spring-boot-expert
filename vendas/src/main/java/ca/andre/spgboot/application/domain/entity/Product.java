08a7eae36d3aec55b8ac878555930ff3a7ba2ed9
Andre <andreoliveira.adv@hotmail.com>
2020-12-25 13:56:53 -0500

    Mapping the Products Entity

--- vendas/src/main/java/ca/andre/spgboot/application/domain/entity/Product.java
+++ vendas/src/main/java/ca/andre/spgboot/application/domain/entity/Product.java
@@ -2,10 +2,26 @@ package ca.andre.spgboot.application.domain.entity;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
 public class Product {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+
+	@Column(name = "description")
 	private String description;
+
+	@Column(name = "unity_price")
 	private BigDecimal price;
 
 	public Integer getId() {
