58ed58e62de0f499c79ad7bc2428c013534a0e57
Andre <andreoliveira.adv@hotmail.com>
2020-12-26 13:24:27 -0500

    Delete Mapping: deleting a resource on the server

--- vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
+++ vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SalesApplication 
 {
-	
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(SalesApplication.class, args);
--- vendas/src/main/java/ca/andre/spgboot/application/rest/controller/CustomerController.java
+++ vendas/src/main/java/ca/andre/spgboot/application/rest/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,38 +16,50 @@ import ca.andre.spgboot.application.domain.entity.Customer;
 import ca.andre.spgboot.application.domain.repository.CustomerRepository;
 
 @Controller
-public class CustomerController 
-{
+public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRepository;
-	
+
 	public CustomerController(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
-	
+
 	@GetMapping("/api/customer/{id}")
 	@ResponseBody
-	public ResponseEntity<Customer> getCustomerById(@PathVariable("id") Integer id) 
-	{
+	public ResponseEntity<Customer> getCustomerById(@PathVariable("id") Integer id) {
 		Optional<Customer> customer = customerRepository.findById(id);
-		
-		if(customer.isPresent()) 
-		{
+
+		if (customer.isPresent()) {
 			return ResponseEntity.ok(customer.get());
 		} else {
-			
+
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
-	
+
 	@PostMapping("/api/customer")
 	@ResponseBody
-	public ResponseEntity<Customer> save(@RequestBody Customer customer) 
-	{
+	public ResponseEntity<Customer> save(@RequestBody Customer customer) {
 		Customer savedCustomer = customerRepository.save(customer);
-		
+
 		return ResponseEntity.ok(savedCustomer);
 	}
-	
+
+	@DeleteMapping("/api/customer/{id}")
+	@ResponseBody
+	public ResponseEntity<Customer> delete(@PathVariable("id") Integer id) {
+
+		Optional<Customer> customer = customerRepository.findById(id);
+
+		if (customer.isPresent()) {
+			customerRepository.delete(customer.get());
+			
+			return ResponseEntity.noContent().build();
+		} else {
+
+			return ResponseEntity.notFound().build();
+		}
+
+	}
+
 }
