8e2dff3a848b0a9f93a32aaf0fdb8cd9c6a32937
Andre <andreoliveira.adv@hotmail.com>
2021-01-01 19:00:47 -0500

    Generating the WAR file

--- vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
+++ vendas/src/main/java/ca/andre/spgboot/application/SalesApplication.java
@@ -2,9 +2,10 @@ package ca.andre.spgboot.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SalesApplication 
+public class SalesApplication extends SpringBootServletInitializer 
 {
 	public static void main(String[] args) 
 	{
